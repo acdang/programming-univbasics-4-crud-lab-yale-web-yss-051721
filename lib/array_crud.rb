00718565945cb031ef041e7967448d09f68d4ebe
ambitious-caller-8426 <anhcdang@gmail.com>
2021-05-23 22:21:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["tea", "coffee", "soda", "water"]
+  drinks = ["tea", "coffee", "soda", "water"]
 end
 
 def add_element_to_end_of_array(array, element)
